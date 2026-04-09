f41fa74e81bdb68903b88ebee6a3f02d2880ba34
Emil Heilbo <emil@heilbo.dev>
2026-04-09 11:26:24 +0200

    build: take positional argument in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
-read -p "Enter assignment number to compile and run: " i
+if [ -z $1 ]; then
+    read -p "Enter assignment number to compile and run: " i
+else
+    i=$1
+fi
 cobc -x -Wall -o build/A${i} -I include -L lib src/ASSIGNMENT-${i}.COB && /usr/bin/env bash -c "./build/A${i}"
